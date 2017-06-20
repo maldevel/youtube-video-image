77a80c683ea02733d71ad79e23a089a2ab60d4cd
maldevel <maldevel@mail.com>
2017-06-20 12:49:43 +0300

    license

--- get-youtube-video-images.py
+++ get-youtube-video-images.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""
+    This file is part of youtube-video-image
+    Copyright (C) 2017 @maldevel
+    https://github.com/maldevel/youtube-video-image
+    
+    get-youtube-video-images.py - Download youtube video cover image.
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    For more see the file 'LICENSE' for copying permission.
+"""
+
+__author__ = "maldevel"
+__copyright__ = "Copyright (c) 2017 @maldevel"
+__credits__ = ["maldevel", "rkmylo"]
+__license__ = "GPLv3"
+__version__ = "0.2"
+__maintainer__ = "maldevel"
+
+
+#####################
 
 import os
 import re
